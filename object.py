03660d0df0117a5cdc116abca04089e4e7ab8bed
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2019-12-15 16:54:18 +0100

    working bmesh node

--- object.py
+++ object.py
@@ -26,8 +26,7 @@ class pvObjectNode(bpy.types.Node):
         socket = self.inputs["Input"]
         if socket.is_linked and len(socket.links) > 0:
             other = socket.links[0].from_socket.node
-            other.load_data()
-            pv = other.data.pv
+            pv = other.pv()
             print("Fetching data from",pv,"...")
             pvd = paraview.servermanager.Fetch(pv)
             self.make_mesh(pvd)
@@ -42,42 +41,37 @@ class pvObjectNode(bpy.types.Node):
 
 import bmesh
 
-class pvBMeshNode(bpy.types.Node, pvDataNode):
+class pvBMeshNode(bpy.types.Node):
     bl_label = "Blender Object"
-    obName = bpy.props.StringProperty(default="VTKObj")
+    mshName = bpy.props.StringProperty(default="")
     def init(self, context):
         print("Init node: ", self.name)
-        me = bpy.data.meshes.new(self.obName + "Mesh")
-        ob = bpy.data.objects.new(self.obName, me)
-        self.obName = ob.name
-        bpy.context.scene.objects.link(ob)
-        self.load_data()
         self.inputs.new("pvNodeSocket", "Input")
     def init_data(self):
         self.data.ob = bpy.data.objects[self.obName]
         self.data.bm = bmesh.new()
         self.data.bm.from_mesh(self.data.ob.data)
     def update(self):
-        self.load_data()
         print("Updating node: ", self.name)
         socket = self.inputs["Input"]
         if socket.is_linked and len(socket.links) > 0:
             other = socket.links[0].from_socket.node
-            other.load_data()
-            pv = other.data.pv
+            pv = other.pv()
             print("Fetching data from",pv,"...")
             pvd = paraview.servermanager.Fetch(pv)
-            self.make_mesh(pvd)
-    def make_mesh(self,d):
+            if self.mshName in bpy.data.meshes:
+                self.make_mesh(pvd, bpy.data.meshes[self.mshName])
+    def make_mesh(self,d,msh):
         if type(d) == vtkCommonDataModelPython.vtkPolyData:
-            self.make_polydata(d)
-    def make_polydata(self,pdata):
-        bm = self.data.bm
+            self.make_polydata(d,msh)
+        msh.update()
+    def make_polydata(self,pdata,msh):
+        bm = bmesh.new()
         polydata.bmesh_from_polydata(bm,pdata)
-        bm.to_mesh(self.data.ob.data)
-        self.data.ob.data.update()
+        bm.to_mesh(msh)
+        bm.free()
     def draw_buttons(self, context, layout):
-        layout.prop(self, "obName", text="Object name")
+        layout.prop_search(self, "mshName", bpy.data, "meshes", text="Object name")
     def free(self):
         self.free_data()
 
